f555a8fe60458909bf34b27c466d9d11e351a2fa
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2024-10-14 14:06:56 +0200

    wip
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@risson.space>

--- crates/providers-oauth2/src/models.rs
+++ crates/providers-oauth2/src/models.rs
@@ -88,3 +88,59 @@ pub mod access_token {
     impl ActiveModelBehavior for ActiveModel {}
 }
 pub use access_token::Entity as AccessToken;
+
+pub mod refresh_token {
+    use authentik_orm_utils::prelude::*;
+    use sea_orm::entity::prelude::*;
+    use serde::{Deserialize, Serialize};
+
+    use crate::id_token::IDToken;
+    #[expiring_model]
+    #[derive(
+        Clone, Debug, PartialEq, Eq, DeriveEntityModel, Serialize, Deserialize, ExpiringModel, ExpiringModelAction,
+    )]
+    #[sea_orm(table_name = "authentik_providers_oauth2_refreshtoken")]
+    pub struct Model {
+        #[sea_orm(primary_key)]
+        #[serde(skip_deserializing)]
+        pub id: i32,
+        pub provider_id: i32,
+        pub revoked: bool,
+        #[sea_orm(column_type = "Text")]
+        pub _scope: String,
+
+        #[sea_orm(column_type = "Text")]
+        pub token: String,
+        #[sea_orm(column_type = "Text")]
+        pub _id_token: String,
+    }
+
+    #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
+    pub enum Relation {
+        #[sea_orm(
+            belongs_to = "super::oauth2_provider::Entity",
+            from = "Column::ProviderId",
+            to = "super::oauth2_provider::Column::ProviderPtrId"
+        )]
+        OAuth2Provider,
+    }
+
+    impl Related<super::oauth2_provider::Entity> for Entity {
+        fn to() -> RelationDef {
+            Relation::OAuth2Provider.def()
+        }
+    }
+
+    impl Model {
+        pub(crate) fn get_scope(&self) -> Vec<&str> {
+            self._scope.split(' ').collect()
+        }
+
+        pub(crate) fn get_id_token(&self) -> serde_json::Result<IDToken> {
+            Ok(serde_json::from_str(&self._id_token)?)
+        }
+    }
+
+    impl ActiveModelBehavior for ActiveModel {}
+}
+pub use refresh_token::Entity as RefreshToken;
--- crates/providers-oauth2/src/views.rs
+++ crates/providers-oauth2/src/views.rs
@@ -6,7 +6,7 @@ use serde::{Deserialize, Serialize};
 
 use crate::{
     id_token::IDToken,
-    models::{AccessToken, access_token},
+    models::{AccessToken, access_token, RefreshToken, refresh_token},
     utils::authenticate_provider,
 };
 
@@ -64,6 +64,23 @@ pub(crate) async fn token_introspection(
                     id_token: Some(id_token),
                 }));
             }
+            return Ok(Json(Response::error()))
+        }
+        if let Some(refresh_token) = RefreshToken::find()
+            .filter(refresh_token::Column::Token.eq(&params.token))
+            .filter(refresh_token::Column::ProviderId.eq(provider.provider_ptr_id))
+            .one(&db)
+            .await?
+        {
+            if let Ok(id_token) = refresh_token.get_id_token() {
+                return Ok(Json(Response {
+                    active: !refresh_token.is_expired() && !refresh_token.revoked,
+                    scope: Some(refresh_token.get_scope().join(" ")),
+                    client_id: Some(provider.client_id),
+                    id_token: Some(id_token),
+                }));
+            }
+            return Ok(Json(Response::error()))
         }
     }
     Ok(Json(Response::error()))
